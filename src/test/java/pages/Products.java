6069e6968d56924d41f83310e03579565a5ae5b7
ebuzer <ebuzer.tutuk@gmail.com>
2022-01-09 23:51:07 +0200

    Added locators by the ETT-56

--- src/test/java/pages/Products.java
+++ src/test/java/pages/Products.java
@@ -1,4 +1,20 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Drivers;
+
 public class Products {
+
+    Products() {
+        PageFactory.initElements(Drivers.getDriver(),this);
+    }
+
+    @FindBy(css = "li>div[class=\"desc\"]")
+    private WebElement productList;
+
+    public WebElement getProductList() {
+        return productList;
+    }
 }
