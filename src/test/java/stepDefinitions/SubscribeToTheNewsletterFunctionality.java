9ec4905b7c18d1b30f01c84d67289b3e56212025
kurucayerdal <kurucayerdal@gmail.com>
2022-01-16 20:05:37 +0100

    mission is completed for ETT-14 branch

--- src/test/java/stepDefinitions/SubscribeToTheNewsletterFunctionality.java
+++ src/test/java/stepDefinitions/SubscribeToTheNewsletterFunctionality.java
@@ -1,11 +1,12 @@
 package stepDefinitions;
 
 import cucumber.api.java.en.*;
-import net.bytebuddy.utility.RandomString;
+import org.testng.Assert;
 import pages.Home;
+import utils.Drivers;
+import utils.Parent;
 
-
-public class SubscribeToTheNewsletterFunctionality {
+public class SubscribeToTheNewsletterFunctionality extends Parent {
 
     private Home home = new Home();
 
@@ -15,32 +16,61 @@ public class SubscribeToTheNewsletterFunctionality {
 
     @Given("^Type an \"([^\"]*)\" into the email input on the top of the page$")
     public void typeAnIntoTheEmailInputOnTheTopOfThePage(String email) {
-        home.getHome_e_mail_input().sendKeys(email+randomEmailGenerator());
+        sendKeys(home.getHome_e_mail_input(), email);
     }
 
     @And("^Click on the Jetzt abonnieren button$")
     public void clickOnTheJetztAbonnierenButton() {
-    }
-
-    @Then("^The user should see the error alert with invalid e-mail or success message with valid e-mail$")
-    public void theUserShouldSeeTheErrorAlertWithInvalidEMailOrSuccessMessageWithValidEMail() {
+        clickElement(home.getHome_e_mail_submit());
     }
 
     @Given("^Type an \"([^\"]*)\" into the email input on the bottom of the page$")
     public void typeAnIntoTheEmailInputOnTheBottomOfThePage(String email) {
-
+        sendKeys(home.getHome_e_mail_bottom_input(), email);
     }
 
-    @And("^Click on the  Abone Ol button$")
-    public void clickOnTheAboneOlButton() {
+    @And("^Click on the registrieren button$")
+    public void clickOnTheRegistrierenButton() {
+        clickElement(home.getHome_e_mail_bottom_senden());
     }
 
-    @Given("^Type an \"([^\"]*)\" into the email input on the botoom of the page$")
-    public void typeAnIntoTheEmailInputOnTheBotoomOfThePage(String email) {
-
+    @Then("^The user should see the error alert with invalid \"([^\"]*)\" or success message with valid e-mail for bottom registration$")
+    public void theUserShouldSeeTheErrorAlertWithInvalidOrSuccessMessageWithValidEMailForBottomRegistration(String email) {
+        if (email.contains("@gmail.com")) {
+            if (Drivers.getDriver().getCurrentUrl().equals("https://www.mevlanacay.de/")) {
+                assertMessage(home.getHome_e_mail_bottom_successMessage(), "Dankeschön, Deine Anmeldung war erfolgreich!");
+            } else {
+                assertMessage(home.getHome_e_mail_bottom_successMessage(), "Teşekkürler, kayıt başvurunuz başarıyla tamamlandı!");
+            }
+        } else {
+            Assert.assertTrue(home.getHome_e_mail_input().getAttribute("validationMessage")
+                    .contains("Lütfen e-posta adresine bir \"@\" işareti") ||
+                    home.getHome_e_mail_input().getAttribute("validationMessage")
+                            .contains("Lütfen \"@\" işaretinden sonra gelen kısmı") ||
+                    home.getHome_e_mail_input().getAttribute("validationMessage")
+                            .contains("Lütfen bu alanı doldurun") ||
+                    home.getHome_e_mail_input().getAttribute("validationMessage")
+                            .contains(""));
+        }
     }
 
-    private String randomEmailGenerator(){
-        return RandomString.make(5);
+    @Then("^The user should see the error alert with invalid \"([^\"]*)\" or success message with valid e-mail for top registration$")
+    public void theUserShouldSeeTheErrorAlertWithInvalidOrSuccessMessageWithValidEMailForTopRegistration(String email) {
+        if (email.contains("@gmail.com")) {
+            if (Drivers.getDriver().getCurrentUrl().equals("https://www.mevlanacay.de/")) {
+                assertMessage(home.getHome_e_mail_bar_menu_message(), "Sie haben sich erfolgreich registriert! Vielen Dank. Bitte überprüfen Sie die Bestätigungs-E-Mail, die wir Ihnen gesendet haben.");
+            } else {
+                assertMessage(home.getHome_e_mail_bar_menu_message(), "Başarıyla kaydoldunuz. Teşekkürler.");
+            }
+        } else {
+            Assert.assertTrue(home.getHome_e_mail_input().getAttribute("validationMessage")
+                    .contains("Lütfen e-posta adresine bir \"@\" işareti") ||
+                    home.getHome_e_mail_input().getAttribute("validationMessage")
+                            .contains("Lütfen \"@\" işaretinden sonra gelen kısmı") ||
+                    home.getHome_e_mail_input().getAttribute("validationMessage")
+                            .contains("Lütfen bu alanı doldurun") ||
+                    home.getHome_e_mail_input().getAttribute("validationMessage")
+                            .contains(""));
+        }
     }
 }
--- src/test/resources/featureFiles/SubscribeToTheNewsletter.feature
+++ src/test/resources/featureFiles/SubscribeToTheNewsletter.feature
@@ -1,50 +1,25 @@
-
 Feature: Subscribe To The Company's Newsletter
 
-  Scenario Outline: The user should be able to subscribe to the company's newsletter from "DE" page
+  Scenario Outline: The user should be able to subscribe to the company's newsletter
     Given Type an "<e-mail>" into the email input on the top of the page
     And Click on the Jetzt abonnieren button
-    Then The user should see the error alert with invalid e-mail or success message with valid e-mail
+    Then The user should see the error alert with invalid "<e-mail>" or success message with valid e-mail for top registration
     Examples:
-      | e-mail               |
-      | test@email           |
-      | test@                |
-      | test                 |
-      |                      |
-      | randomtest@gmail.com |
+      | e-mail                |
+      | test@email            |
+      | test@                 |
+      | test                  |
+      |                       |
+      | randomtest3@gmail.com |
 
-  Scenario Outline: The user should be able to subscribe to the company's newsletter from "DE" page
+  Scenario Outline: The user should be able to subscribe to the company's newsletter
     Given Type an "<e-mail>" into the email input on the bottom of the page
-    And Click on the Jetzt abonnieren button
-    Then The user should see the error alert with invalid e-mail or success message with valid e-mail
+    And Click on the registrieren button
+    Then The user should see the error alert with invalid "<e-mail>" or success message with valid e-mail for bottom registration
     Examples:
-      | e-mail               |
-      | test@email           |
-      | test@                |
-      | test                 |
-      |                      |
-      | randomtest@gmail.com |
-
-  Scenario Outline: The user should be able to subscribe to the company's newsletter from "TR" page
-    Given Type an "<e-mail>" into the email input on the top of the page
-    And Click on the  Abone Ol button
-    Then The user should see the error alert with invalid e-mail or success message with valid e-mail
-    Examples:
-      | e-mail               |
-      | test@email           |
-      | test@                |
-      | test                 |
-      |                      |
-      | randomtest@gmail.com |
-
-  Scenario Outline: The user should be able to subscribe to the company's newsletter from "TR" page
-    Given Type an "<e-mail>" into the email input on the botoom of the page
-    And Click on the  Abone Ol button
-    Then The user should see the error alert with invalid e-mail or success message with valid e-mail
-    Examples:
-      | e-mail               |
-      | test@email           |
-      | test@                |
-      | test                 |
-      |                      |
-      | randomtest@gmail.com |
+      | e-mail                |
+      | test@email            |
+      | test@                 |
+      | test                  |
+      |                       |
+      | randomtest1@gmail.com |
